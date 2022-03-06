d22a2c9ec1c4881d39b8338d457cdf092f962113
AbnerDoidao <101028166+AbnerDoidao@users.noreply.github.com>
2022-03-06 01:01:33 -0300

    Create cryptofarms.js

---
+++ cryptofarms.js
@@ -0,0 +1,20 @@
+let clicar = setInterval(function() {
+    var mineSelectorList = document.querySelectorAll(".sc-fzqNqU.kikdAh");
+    loop(mineSelectorList.length - 1)
+}, 10000)
+
+function loop(i){
+    setTimeout(function(){
+        var mineSelectorList = document.querySelectorAll(".sc-fzqNqU.kikdAh");
+        if(mineSelectorList[i].offsetHeight > 0){
+            mineSelectorList[i].click()
+            setTimeout(function(){
+                document.getElementById("mine-equipament").click()
+            }, 500)
+        }
+        i--
+        if(i >= 0 ){
+            loop(i)
+        }  
+    }, 2000)
+}
